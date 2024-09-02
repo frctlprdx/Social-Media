4f4ef7d09db44fdeb4e153f11c72b89da5b1abfe
Ivan Putra Pratama <ifam4@yahoo.com>
2024-09-02 14:14:23 +0700

    Initial Commit

---
+++ Social Media/Social_MediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Social_MediaApp.swift
+//  Social Media
+//
+//  Created by Helena on 02/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Social_MediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
